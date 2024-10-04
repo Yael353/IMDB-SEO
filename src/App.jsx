c292458529604e4f65b5efdd01f34d8a54a64aa6
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2024-10-04 10:09:29 +0200

    SingleMoviePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/hero/Hero";
 import MoviesOutput from "./components/movies/MoviesOutput";
+import SingleMoviePage from "./components/movies/SingleMoviePage";
 
 function App() {
   return (
     <>
       <Hero />
       <MoviesOutput />
+      <SingleMoviePage />
     </>
   );
 }
